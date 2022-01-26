ee6a76cb75371a18d264a7f130ed577a5a374db7
Eduards <eduards.polis2@gmail.com>
2022-01-26 16:19:32 +0200

    Izveidota switch komanda prieks pasutijuma

--- src/picerija.java
+++ src/picerija.java
@@ -1,24 +1,49 @@
 import javax.swing.JOptionPane;
 
 public class picerija {
-
-	Pica pica;
 	
-	public void pasutitPicu() {
+	Pica pica;
+	double cena;
+	static double rekins=0;
+	static int picasIzm;
+	static int skaits=0;
+	
+	public static double pasutitPicu1(double rekins) {
 		
+		
+		do {
+			picasIzm = Integer.parseInt(JOptionPane.showInputDialog(null, "Cik lielu picu vçlies pasûtit? \n 20cm $6,59 \n 30cm $7,59 \n 50cm $9,99 \n "));
+		switch(picasIzm) {
+		case 20:
+			rekins = rekins + 6.59;
+			break;
+		case 30:
+			rekins = rekins + 7.59;
+			break;
+		case 50:
+			rekins = rekins + 9.99;
+			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Lielums neatbilst", null, JOptionPane.ERROR_MESSAGE);
+		}
+		}while(picasIzm !=20 && picasIzm !=30 && picasIzm !=50);
+		
+		
+		return rekins;
 	}
 	
 	
 	public static void main(String[] args) {
 		String izvele;
-		double cena, rekins;
+		
 		
 		try {
 		do {
-			izvele = JOptionPane.showInputDialog("[1]Pasûtît picu | [2] Apskatît pasûtîjumu | [3] Sagatavot rçíinu");
+			izvele = JOptionPane.showInputDialog("[1]Pasûtît picu | [2] Apskatît pasûtîjumu | [3] Sagatavot rçíinu | [x] Atcelt");
+			
 		switch(izvele) {
 		case "1":
-			//pasutitPicu();
+			pasutitPicu1(rekins);
 			break;
 		case "2":
 			//apskatitPasutijumu();
@@ -37,6 +62,12 @@ public class picerija {
 			JOptionPane.showMessageDialog(null, "Notika kluda pasûtijuma veikðanâ!");
 		}
 
+	}
+
+
+	private static void pasutitPicu(double rekins2) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
